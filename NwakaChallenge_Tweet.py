3c5468e51d1e3eb7ad8bca84e963d5166681978c
Dora F <doradora.prog@gmail.com>
2018-06-08 09:24:17 +0900

    名詞に修飾語をつけるシステムに変更

--- NwakaChallenge_Tweet.py
+++ NwakaChallenge_Tweet.py
@@ -10,9 +10,12 @@ twitter = OAuth1Session(CK, CS, AT, ATS) #認証処理
 
 url = "https://api.twitter.com/1.1/statuses/update.json" #タイムライン取得エンドポイント
 
-lst = ["食べたい","豊かに","欲しい","止まるんじゃねえぞ"] # 末文
+# lst = ["食べたい","豊かに","欲しい","止まるんじゃねえぞ","遅刻"] # 末文
+part_lst = ["が食べたい","で豊かに","が欲しい","止まるんじゃねえぞ","は遅刻"] # 助詞＋末文
+modi = ["美味しい","美しい", "可愛い", "金持ちの", "天使のような", "酷すぎる"] # 修飾語
 
 nouns = [Nouns("にわか","person"),
+        Nouns("にわな","person"),
         Nouns("金","other"),
         Nouns("焼肉","food"),
         Nouns("人生","other"),
@@ -27,28 +30,34 @@ tweet_length = 10 # 初期値はハッシュタグ分
 is_continue = True
 nwaka_add = False
 
-while is_continue:
-    noun_idx = random.randrange(0, len(nouns))
-    noun_word = nouns.pop(noun_idx)
-    part_word = random.choice(noun_word.next_particle())
+noun_idx = random.randrange(0, len(nouns))
+noun_word = nouns.pop(noun_idx)
+part_word = noun_word.word_with_modifier(nouns, modi)
 
-    tweet_content.append(noun_word.word)
-    tweet_content.append(part_word)
-    tweet_length += len(noun_word.word) + len(part_word)
-    if noun_word.word == "にわか":
-        nwaka_add = True
-    if part_word == "が":
-        tweet_content.append(random.choice(lst))
-        break
+# while is_continue:
+#     noun_idx = random.randrange(0, len(nouns))
+#     noun_word = nouns.pop(noun_idx)
+#     part_word = random.choice(noun_word.next_particle())
+
+#     tweet_content.append(noun_word.word)
+#     tweet_content.append(part_word)
+#     tweet_length += len(noun_word.word) + len(part_word)
+#     if noun_word.word in ["にわか", "にわな"]:
+#         nwaka_add = True
+#     if part_word == "が":
+#         tweet_content.append(random.choice(lst))
+#         break
     
-    r = random.random()
-    if r < (tweet_length / 100) and nwaka_add or len(nouns) == 0:
-        is_continue = False
+#     r = random.random()
+#     if r < (tweet_length / 20) and nwaka_add or len(nouns) == 0:
+#         is_continue = False
 
+# Nwaka = "#にわかちゃれんじ\n"
+# for l in tweet_content:
+#     Nwaka += l
 
-Nwaka = "#にわかちゃれんじ\n"
-for l in tweet_content:
-    Nwaka += l
+Nwaka = "#にわかちゃれんじ\n" + part_word + random.choice(part_lst)
+
 print("{}".format(Nwaka))
 
 yesno = ""
--- words.py
+++ words.py
@@ -12,22 +12,29 @@ class Nouns(Words):
     def __init__(self, word, part):
         super().__init__(word, part)
     
-    def next_particle(self): # 次の助詞を制限する
-        if self.part == "person":
-            return ["の","が","に","へ","と","、"]
-        elif self.part == "place":
-            return ["の","で","を","が","に","へ","と","から","、"]
-        elif self.part == "food":
-            return ["で","を","が","に","へ","と","、"]
-        else:
-            return ["の","で","を","が","に","へ","と","、"]
+    # def next_particle(self): # 次の助詞を制限する
+    #     if self.part == "person":
+    #         return ["の","が","に","へ","と","、"]
+    #     elif self.part == "place":
+    #         return ["の","で","を","が","に","へ","と","から","、"]
+    #     elif self.part == "food":
+    #         return ["で","を","が","に","へ","と","、"]
+    #     else:
+    #         return ["の","で","を","が","に","へ","と","、"]
     
-    def add_modifier(self, nouns, modi): # 修飾語をつける
-    # 実行確認してない
+    def word_with_modifier(self, nouns, modi):
+        return self.add_modifier(nouns, modi, self.word, 0.2)
+
+    def add_modifier(self, nouns, modi, word, per): # 修飾語をつける
         r  = random.random()
-        if r < 0.3:
-            return random.choice(modi) + self.word
-        elif r < 0.6:
-            return random.choice(nouns) + "の" + self.word
+        if r < per:
+            w = random.choice(modi) + word
+        elif r < per * 1.7:
+            w = random.choice(nouns).word + "の" + word
+        elif r < per * 2:
+            w = random.choice(nouns).word + "と" + word
         else:
-            return self.word
+            return word
+        return self.add_modifier(nouns, modi, w, per)
+
+# もう少し単語を細かく分類してより精度を上げていきたい（希望的観測）
